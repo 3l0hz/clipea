25c7c3789d454c85761e76fdc6dd084ad2519f43
Eloy Hernandez D <eloyhernandezduarte@gmail.com>
2026-05-09 19:50:52 +0000

    cambia las imagenes de soporte parabrisas moto por estas https://bwdvsb

--- src/constants/data.ts
+++ src/constants/data.ts
@@ -225,15 +225,15 @@ export const PRODUCTS: Product[] = [
     name: 'Soporte Parabrisas GoPro Insta',
     category: 'Soportes Moto / Vehículo',
     price: '$19.990',
-    image: 'https://bwdvsbxwqlnlzfwfsoid.supabase.co/storage/v1/object/public/Products/ventosa/11.png',
+    image: 'https://bwdvsbxwqlnlzfwfsoid.supabase.co/storage/v1/object/public/Products/soporteparab/primera.png',
     images: [
-      'https://bwdvsbxwqlnlzfwfsoid.supabase.co/storage/v1/object/public/Products/ventosa/11.png',
-      'https://bwdvsbxwqlnlzfwfsoid.supabase.co/storage/v1/object/public/Products/ventosa/1(1).jpg',
-      'https://bwdvsbxwqlnlzfwfsoid.supabase.co/storage/v1/object/public/Products/ventosa/2.jpg'
+      'https://bwdvsbxwqlnlzfwfsoid.supabase.co/storage/v1/object/public/Products/soporteparab/primera.png',
+      'https://bwdvsbxwqlnlzfwfsoid.supabase.co/storage/v1/object/public/Products/soporteparab/1.png',
+      'https://bwdvsbxwqlnlzfwfsoid.supabase.co/storage/v1/object/public/Products/soporteparab/2.png'
     ],
-    description: 'Soporte de succión ultra resistente diseñado para capturar tomas estables desde el parabrisas de tu vehículo.',
+    description: 'Soporte ultra resistente diseñado para capturar tomas estables desde el parabrisas de tu moto o vehículo.',
     compatibility: 'GoPro, Insta360, DJI Action',
-    recommendedUse: 'Vlogs de auto, rutas de carretera',
+    recommendedUse: 'Rutas de carretera, motovlogs',
     bestSeller: false
   },
   // OTROS
